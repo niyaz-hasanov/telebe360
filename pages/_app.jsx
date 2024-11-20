2ecb54bca2ac8f1ddd704a1e54a4ae6a05189a6e
niyaz-hasanov <niyazhasanov30@gmail.com>
2024-11-21 03:39:41 +0400

    Add files via upload

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,13 +4,33 @@ import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { Toaster } from 'react-hot-toast';
+import startTokenRefresh from '../utils/startTokenRefresh'; // Token yenileme fonksiyonunu import et
+import Footer from '../components/footer/index'
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-  const [loading, setLoading] = useState(false); // Yüklenme durumu için state
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const handleStart = () => setLoading(true); // Yüklenme başlat
-    const handleComplete = () => setLoading(false); // Yüklenme tamamla
+    const handleStart = () => setLoading(true);
+    const handleComplete = () => setLoading(false);
+
+    const tokenRefreshStartTime = localStorage.getItem('tokenRefreshStartTime');
+    const currentTime = new Date().getTime();
+    const oneHour = 30 * 60 * 1000; 
+
+    if (!tokenRefreshStartTime || currentTime - tokenRefreshStartTime > oneHour) {
+      startTokenRefresh();
+
+      const interval = setInterval(() => {
+        startTokenRefresh();
+      }, 30 * 60 * 1000);
+
+      localStorage.setItem('tokenRefreshStartTime', currentTime);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }
 
     router.events.on('routeChangeStart', handleStart);
     router.events.on('routeChangeComplete', handleComplete);
@@ -23,7 +43,6 @@ function MyApp({ Component, pageProps }) {
     };
   }, [router]);
 
-  // Navbar'ın gizleneceği sayfalar
   const noNavbarRoutes = [
     '/technical_support',
     '/notifications',
@@ -36,13 +55,33 @@ function MyApp({ Component, pageProps }) {
     '/settings/membership',
     '/settings/notifications',
     '/settings/references',
-  ]; 
+    '/verify_email/[token]',
+    '/ticket_burn/[id]'
+  ];
+  const noFooterRoutes = [
+    '/technical_support',
+    '/notifications',
+    '/login',
+    '/before_login',
+    '/register',
+    '/settings',
+    '/settings/my360id',
+    '/settings/security',
+    '/settings/membership',
+    '/settings/notifications',
+    '/settings/references',
+    '/verify_email/[token]',
+    '/ticket_burn/[id]',
+    '/my_tickets',
+    '/coming_soon'
+   
+  ];
 
   return (
     <>
-      {loading ? ( // Eğer loading true ise yüklenme animasyonu göster
+      {loading ? (
         <div className="loading-spinner">
-         <p className='loading-text'> Səhifə yüklənir. Gözlədiyinizə görə təşəkkür edirik</p>
+          <p className='loading-text'> Səhifə yüklənir. Gözlədiyinizə görə təşəkkür edirik</p>
           <div className="spinner">
             <Image
               width={50}
@@ -56,7 +95,12 @@ function MyApp({ Component, pageProps }) {
         <>
           {!noNavbarRoutes.includes(router.pathname) && <Navbar />} 
           <Component {...pageProps} />
-          <Toaster />
+          <Toaster 
+            toastOptions={{
+              duration: 5000, 
+            }}
+          /> 
+         {!noFooterRoutes.includes(router.pathname) && <Footer/>} 
         </>
       )}
     </>
