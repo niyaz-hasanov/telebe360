00b8ca752847a9cb7f2d73c50e9bed7508f03e86
Niyaz Hasanov <niyazhasanov30@gmail.com>
2025-12-06 14:23:01 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -19,9 +19,10 @@
 // module.exports = transpiledModules(nextConfig);
 
 /** @type {import('next').NextConfig} */
+ @type {import('next').NextConfig} 
 
 const nextConfig = {
-
+  distDir: 'dist', 
   async rewrites() {
     return [
       {
@@ -31,9 +32,9 @@ const nextConfig = {
     ];
   },
   images: {
-    domains: ['api.telebe360.com'],
+    domains: ['api.telebe360.com'], 
   },
 };
 
-const transpiledModules = require('next-transpile-modules')(['react-icons']);
+const transpiledModules = require('next-transpile-modules')(["react-icons"]);
 module.exports = transpiledModules(nextConfig);
